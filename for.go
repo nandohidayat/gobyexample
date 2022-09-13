4cd9fa80d63954adf1b3e4bb2aa1fed65e73147d
nandohidayat <mnandohidayat@gmail.com>
2022-09-13 16:22:37 +0700

    if

---
+++ for.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1
+	for i < 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	println()
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	println()
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	println()
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
